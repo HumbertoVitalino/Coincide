2c04fde3c6c58f99c4001c5b7cf5c6368ab3fc6c
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-10 07:59:39 -0300

    feat: dependency injection

---
+++ backend/Infra/IoC/DependencyInjectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Infra.IoC;
+
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.AddDbContext<CoincideContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("CoincideContext")!);
+        });
+
+        return serviceCollection;
+    }
+}
