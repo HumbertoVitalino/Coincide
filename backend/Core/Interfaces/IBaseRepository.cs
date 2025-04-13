7a514d1adb73a75593080afa0b539249b922b4ee
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-12 22:47:49 -0300

    feat: base repo

---
+++ backend/Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Domain;
+
+namespace Core.Interfaces;
+
+public interface IBaseRepository<T> where T : Entity
+{
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<T> Get(Guid? id, CancellationToken cancellationToken);
+    Task<List<T>> GetAll(CancellationToken cancellationToken);
+}
