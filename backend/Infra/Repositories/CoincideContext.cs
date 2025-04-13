da2ff6cda8b33793557565325a2a8a75bd840311
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-12 22:45:12 -0300

    feat: migration

--- backend/Infra/Repositories/CoincideContext.cs
+++ backend/Infra/Repositories/CoincideContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repositories;
 
 public class CoincideContext : DbContext
 {
     public CoincideContext(DbContextOptions<CoincideContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
 
 }
