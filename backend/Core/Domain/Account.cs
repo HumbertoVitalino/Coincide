c50c7b4eea664c18bce08dafa1016afae5888645
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-15 09:44:04 -0300

    refactor: account entity

--- backend/Core/Domain/Account.cs
+++ backend/Core/Domain/Account.cs
@@ -4,13 +4,19 @@ public class Account : Entity
 {
     public string Name { get; set; }
     public decimal Balance { get; set; }
+    public decimal TotalIncome { get; set; }
+    public decimal TotalExpense {  get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; }
 
-    public Account(string name, decimal balance, Guid userId)
+    public Account() { }
+
+    public Account(string name, decimal balance, decimal totalIncome, decimal totalExpense, Guid userId)
     {
         Name = name;
         Balance = balance;
+        TotalIncome = totalIncome;
+        TotalExpense = totalExpense;
         UserId = userId;
     }
 }
