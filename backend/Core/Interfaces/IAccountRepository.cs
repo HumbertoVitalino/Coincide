03cb56fa2b36e36ce34f9221480ad1e8dc61f751
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-15 09:44:30 -0300

    feat: account repository

--- backend/Core/Interfaces/IAccountRepository.cs
+++ backend/Core/Interfaces/IAccountRepository.cs
@@ -4,4 +4,5 @@ namespace Core.Interfaces;
 
 public interface IAccountRepository : IBaseRepository<Account>
 {
+    Task<Account> GetAsync(Guid userId);
 }
--- backend/Infra/Repositories/AccountRepository.cs
+++ backend/Infra/Repositories/AccountRepository.cs
@@ -1,9 +1,17 @@
 ﻿using Core.Domain;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repositories;
 
 public class AccountRepository : BaseRepository<Account>, IAccountRepository
 {
     public AccountRepository(CoincideContext context) : base(context) { }
+
+    public async Task<Account> GetAsync(Guid userId)
+    {
+        return await _context.Accounts
+            .Include(u => u.User)
+            .FirstOrDefaultAsync(x => x.UserId == userId);
+    }
 }
