dcb4c334ccc9146ca7725fc4ce9bd8b0176a4a81
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-16 01:40:12 -0300

    feat: controller

---
+++ backend/Api/Controllers/v1/TransactionController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Controllers.Models;
+using Api.Mapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.v1;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Authorize]
+public class TransactionController(
+    IMediator mediator
+) : BaseController
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpPost("NewTransaction")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateAsync([FromBody] NewTransactionRequest request, CancellationToken cancellationToken)
+    {
+        var input = request.MapToInput(UserId);
+
+        var output = await _mediator.Send(input, cancellationToken);
+
+        if (output.IsValid)
+            return Ok(output);
+
+        return BadRequest(output);
+    }
+}
