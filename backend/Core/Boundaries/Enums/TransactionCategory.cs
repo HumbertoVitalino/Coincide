69529ef63274679f04dcd9672ee214c83dcd8fa1
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-16 01:38:49 -0300

    feat: enums

---
+++ backend/Core/Boundaries/Enums/TransactionCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Boundaries.Enums;
+
+public enum TransactionCategory
+{
+    Food,
+    Transportation,
+    Health,
+    Entertainment,
+    Housing,
+    Education,
+    Shopping,
+    Travel,
+    Bills,
+    Other
+}
