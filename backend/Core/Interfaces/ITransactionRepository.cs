1c2361d5fea443ca7bc2ebf652217b427d28c04c
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-19 08:58:10 -0300

    feat: repository

--- backend/Core/Interfaces/ITransactionRepository.cs
+++ backend/Core/Interfaces/ITransactionRepository.cs
@@ -4,4 +4,5 @@ namespace Core.Interfaces;
 
 public interface ITransactionRepository : IBaseRepository<Transaction>
 {
+    Task<IEnumerable<Transaction>> GetAllAsync(Guid UserId, CancellationToken cancellationToken);
 }
