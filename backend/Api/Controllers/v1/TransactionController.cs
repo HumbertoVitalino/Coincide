d1e9ad62f22fb1bad404dfabe05330b8ae16c140
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-19 08:58:30 -0300

    feat: controller

--- backend/Api/Controllers/v1/TransactionController.cs
+++ backend/Api/Controllers/v1/TransactionController.cs
@@ -1,5 +1,7 @@
 ﻿using Api.Controllers.Models;
 using Api.Mapper;
+using Core.Commons;
+using Core.UseCase.GetTransactions.Boundaries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,9 +17,25 @@ public class TransactionController(
 {
     private readonly IMediator _mediator = mediator;
 
+    [HttpGet("GetAll")]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var input = new GetTransactionsInput(UserId);
+
+        var output = await _mediator.Send(input, cancellationToken);
+
+        if (output.IsValid)
+            return Ok(output);
+
+        return BadRequest(output);
+    }
+
     [HttpPost("NewTransaction")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> CreateAsync([FromBody] NewTransactionRequest request, CancellationToken cancellationToken)
     {
