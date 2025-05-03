c9b4dcfeab91bec81b63037717ad559887eb26c5
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-02 23:26:07 -0300

    feat new user

--- backend/Core/Domain/User.cs
+++ backend/Core/Domain/User.cs
@@ -3,11 +3,12 @@
 public class User : Entity
 {
     public string Name { get; set; }
+    public DateTime Birthday { get; set; }
     public string Email { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
-    public List<Account> Accounts { get; set; } = new List<Account>();
-    public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    public double Balance { get; set; } = 0.0;
+
 
     public User() {}
     public User(string name, string email, byte[] passwordHash, byte[] passwordSalt)
