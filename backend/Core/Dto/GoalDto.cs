87e68f4ca686cd5478d3ff8dad4d2b9e04d16ca8
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-05 19:26:52 -0300

    feat: get goals

---
+++ backend/Core/Dto/GoalDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Dto;
+
+public sealed record GoalDto(
+    Guid Id,
+    string Title,
+    decimal TargetAmount,
+    string? Description,
+    DateTime StartDate,
+    DateTime EndDate,
+    decimal MonthlyExpectedValue
+);
---
+++ backend/Core/UseCase/GetGoalsByUserUseCase/Boundaries/GetGoalsByUserInput.cs
@@ -0,0 +1,6 @@
+﻿using Core.Commons;
+using MediatR;
+
+namespace Core.UseCase.GetGoalsByUserUseCase.Boundaries;
+
+public sealed record GetGoalsByUserInput(Guid Id) : IRequest<Output>;
