f45d97baca7deb70d1db132b0b5b913616020439
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-05 19:26:42 -0300

    feat: new goal usecase

---
+++ backend/Core/UseCase/NewGoalUseCase/NewGoal.cs
@@ -0,0 +1,29 @@
+﻿using Core.Commons;
+using Core.Interfaces;
+using Core.Mappers;
+using Core.UseCase.NewGoalUseCase.Boundaries;
+using MediatR;
+
+namespace Core.UseCase.NewGoalUseCase;
+
+public class NewGoal(
+    IGoalRepository goalRepository,
+    IUnitOfWork unitOfWork
+) : IRequestHandler<NewGoalInput, Output>
+{
+    private readonly IGoalRepository _goalRepository = goalRepository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task<Output> Handle(NewGoalInput input, CancellationToken cancellationToken)
+    {
+        var output = new Output();
+
+        var goal = input.MapToEntity();
+
+        _goalRepository.Create(goal);
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        output.AddResult(goal.MapToDto());
+        return output;
+    }
+}
