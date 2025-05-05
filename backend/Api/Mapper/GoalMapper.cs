57b6a22138b4814603bda7663a5fcb44a4b5e302
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-05 19:25:45 -0300

    feat: mapper

---
+++ backend/Api/Mapper/GoalMapper.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models;
+using Core.UseCase.NewGoalUseCase.Boundaries;
+
+namespace Api.Mapper;
+
+public static class GoalMapper
+{
+    public static NewGoalInput MapToInput(this NewGoalRequest request, Guid userId)
+    {
+        return new NewGoalInput(
+            request.Title,
+            request.TargetAmount,
+            request.Description,
+            request.StartDate,
+            request.EndDate,
+            request.MonthlyExpectedValue,
+            userId);
+    }
+}
---
+++ backend/Core/Mappers/GoalMapper.cs
@@ -0,0 +1,34 @@
+﻿using Core.Domain;
+using Core.Dto;
+using Core.UseCase.NewGoalUseCase.Boundaries;
+
+namespace Core.Mappers;
+
+public static class GoalMapper
+{
+    public static Goal MapToEntity(this NewGoalInput input)
+    {
+        return new Goal(
+            input.Title,
+            input.TargetAmount,
+            input.Description,
+            input.StartDate,
+            input.EndDate,
+            input.MonthlyExpectedValue,
+            input.userId
+        );
+    }
+
+    public static GoalDto MapToDto(this Goal goal)
+    {
+        return new GoalDto(
+            goal.Id,
+            goal.Title,
+            goal.TargetAmount,
+            goal.Description,
+            goal.StartDate,
+            goal.EndDate,
+            goal.MonthlyExpectedValue
+        );
+    }
+}
