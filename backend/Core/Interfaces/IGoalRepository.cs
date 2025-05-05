1bc89bdb0464e7792666f0413481743d9c604aad
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-05 19:24:12 -0300

    feat: interface-repository

---
+++ backend/Core/Interfaces/IGoalRepository.cs
@@ -0,0 +1,8 @@
+﻿using Core.Domain;
+
+namespace Core.Interfaces;
+
+public interface IGoalRepository : IBaseRepository<Goal>
+{
+    Task<IEnumerable<Goal>> GetByUserId(Guid userId);
+}
---
+++ backend/Infra/Repositories/GoalRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Domain;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories;
+
+public class GoalRepository : BaseRepository<Goal>, IGoalRepository
+{
+    public GoalRepository(CoincideContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Goal>> GetByUserId(Guid userId)
+    {
+        return await _context.Goals
+            .Include(t => t.User)
+            .Where(t => t.User.Id == userId)
+            .ToListAsync();
+    }
+}
