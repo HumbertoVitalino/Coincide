60300447ef87b4f7239482167b2146f310cd2770
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-05 19:26:34 -0300

    feat: new goal boundaries

---
+++ backend/Core/UseCase/NewGoalUseCase/Boundaries/NewGoalInput.cs
@@ -0,0 +1,14 @@
+﻿using Core.Commons;
+using MediatR;
+
+namespace Core.UseCase.NewGoalUseCase.Boundaries;
+
+public sealed record NewGoalInput(
+    string Title,
+    decimal TargetAmount,
+    string? Description,
+    DateTime StartDate,
+    DateTime EndDate,
+    decimal MonthlyExpectedValue,
+    Guid userId
+) : IRequest<Output>;
