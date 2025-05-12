f48d43f9aeeb1beb5bc536ec7dad98e280a984ab
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-11 23:34:15 -0300

    feat: boundaries

---
+++ backend/Api/Models/NewIncomeRequest.cs
@@ -0,0 +1,11 @@
+﻿using Core.Domain.Enums;
+
+namespace Api.Models;
+
+public sealed record NewIncomeRequest(
+    string Title,
+    decimal Value,
+    string? Description,
+    IncomeType Type,
+    DateTime Date
+);
---
+++ backend/Core/Dto/IncomeDto.cs
@@ -0,0 +1,14 @@
+﻿using Core.Domain.Enums;
+
+namespace Core.Dto;
+
+public record IncomeDto(
+    Guid Id,
+    string Title,
+    decimal Value,
+    string? Description,
+    IncomeType Type,
+    DateTime Date,
+    decimal UserBalance,
+    decimal UserTotalIncome
+);
