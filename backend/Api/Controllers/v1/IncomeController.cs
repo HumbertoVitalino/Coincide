9711d2da921593ad9bfae53f5c3402d78a7ade18
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-11 23:35:35 -0300

    feat: controller

---
+++ backend/Api/Controllers/v1/IncomeController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Mapper;
+using Core.Commons;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.v1;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Authorize]
+public sealed class IncomeController(
+    IMediator mediator
+) : BaseController
+{
+    private readonly IMediator _mediator = mediator;
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> PostAsync([FromBody] Models.NewIncomeRequest request, CancellationToken cancellationToken)
+    {
+        var input = request.MapToInput(UserId);
+
+        var output = await _mediator.Send(input, cancellationToken);
+
+        if (output.IsValid)
+            return Ok(output);
+
+        return BadRequest(output);
+    }
+}
