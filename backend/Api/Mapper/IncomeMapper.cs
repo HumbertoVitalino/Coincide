a29609e3e14fe53c17aab679ef6949bdb94fb67e
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-11 23:33:10 -0300

    feat: mappers

---
+++ backend/Api/Mapper/IncomeMapper.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models;
+using Core.UseCase.NewIncomeUseCase.Boundaries;
+
+namespace Api.Mapper;
+
+public static class IncomeMapper
+{
+    public static NewIncomeInput MapToInput(this NewIncomeRequest request, Guid userId)
+    {
+        return new NewIncomeInput(
+            request.Title,
+            request.Value,
+            request.Description,
+            request.Type,
+            request.Date,
+            userId
+        );
+    }
+}
---
+++ backend/Core/Mappers/IncomeMapper.cs
@@ -0,0 +1,33 @@
+﻿using Core.Domain;
+using Core.Dto;
+using Core.UseCase.NewIncomeUseCase.Boundaries;
+
+namespace Core.Mappers;
+
+public static class IncomeMapper
+{
+    public static Income MapToEntity(this NewIncomeInput input)
+    {
+        return new Income(
+            input.Title,
+            input.Value,
+            input.Type,
+            input.Date,
+            input.UserId
+        );
+    }
+
+    public static IncomeDto MapToDto(this Income income, User user)
+    {
+        return new IncomeDto(
+            income.Id,
+            income.Title,
+            income.Value,
+            income.Description,
+            income.Type,
+            income.Date,
+            user.Balance,
+            user.TotalIncome
+        );
+    }
+}
