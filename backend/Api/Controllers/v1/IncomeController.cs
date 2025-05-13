e9d5887514ffc4cdadcc048a7367f4595bf93438
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-12 21:27:26 -0300

    feat: getall controller

--- backend/Api/Controllers/v1/IncomeController.cs
+++ backend/Api/Controllers/v1/IncomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Api.Mapper;
+using Api.Models;
 using Core.Commons;
+using Core.UseCase.GetAllIncomesUseCase.Boundaries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,12 +16,39 @@ public sealed class IncomeController(
 ) : BaseController
 {
     private readonly IMediator _mediator = mediator;
-    
+
+    /// <summary>  
+    /// Retrieves all income entries for the authenticated user.  
+    /// </summary>  
+    /// <param name="cancellationToken">Token to monitor for cancellation requests.</param>  
+    /// <returns>Returns an Output containing the operation result.</returns>
+    [HttpGet("GetAll")]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var input = new GetAllIncomesInput(UserId);
+
+        var output = await _mediator.Send(input, cancellationToken);
+
+        if (output.IsValid)
+            return Ok(output);
+
+        return BadRequest(output);
+    }
+
+    /// <summary>  
+    /// Handles the creation of a new income entry.  
+    /// </summary>  
+    /// <param name="request">The request object containing the details of the new income.</param>  
+    /// <param name="cancellationToken">Token to monitor for cancellation requests.</param>  
+    /// <returns>Returns an IActionResult containing the operation result.</returns>
     [HttpPost]
     [ProducesResponseType(typeof(Output), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(Output), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> PostAsync([FromBody] Models.NewIncomeRequest request, CancellationToken cancellationToken)
+    public async Task<IActionResult> PostAsync([FromBody] NewIncomeRequest request, CancellationToken cancellationToken)
     {
         var input = request.MapToInput(UserId);
 
