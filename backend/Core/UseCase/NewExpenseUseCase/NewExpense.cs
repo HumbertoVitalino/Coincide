61be915f1a88e6607ad5e929ac5dd6a65c8c5f48
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-24 20:25:54 -0300

    feat: uc

---
+++ backend/Core/UseCase/NewExpenseUseCase/NewExpense.cs
@@ -0,0 +1,35 @@
+﻿using Core.Commons;
+using Core.Interfaces;
+using Core.Mappers;
+using Core.UseCase.NewExpenseUseCase.Boundaries;
+using MediatR;
+
+namespace Core.UseCase.NewExpenseUseCase;
+
+public class NewExpense(
+    IExpenseRepository expenseRepository,
+    IUnitOfWork unitOfWork,
+    IUserRepository userRepository
+) : IRequestHandler<NewExpenseInput, Output>
+{
+    private readonly IExpenseRepository _expenseRepository = expenseRepository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IUserRepository _userRepository = userRepository;
+
+    public async Task<Output> Handle(NewExpenseInput input, CancellationToken cancellationToken)
+    {
+        Output output = new();
+
+        var user = await _userRepository.Get(input.UserId, cancellationToken);
+        var expense = input.MapToEntity();
+
+        user.Balance -= expense.Value;
+        user.TotalExpense -= expense.Value;
+
+        _expenseRepository.Create(expense);
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        output.AddResult(expense.MapToDto());
+        return output;
+    }
+}
