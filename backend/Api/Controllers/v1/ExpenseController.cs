0af6c34e5714bd215fe798c3268c03ad56648a21
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-24 20:26:21 -0300

    feat: controller

---
+++ backend/Api/Controllers/v1/ExpenseController.cs
@@ -0,0 +1,40 @@
+﻿using Api.Mapper;
+using Api.Models;
+using Core.Commons;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.v1;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Authorize]
+public sealed class ExpenseController(
+    IMediator mediator    
+) : BaseController
+{
+    private readonly IMediator _mediator = mediator;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> PostAsync([FromBody] NewExpenseRequest request, CancellationToken cancellationToken)
+    {
+        var input = request.MapToInput(UserId);
+
+        var output = await _mediator.Send(input, cancellationToken);
+
+        if(output.IsValid)
+            return Ok(output);
+
+        return BadRequest(output);
+    }
+}
