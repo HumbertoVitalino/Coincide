1864080dd5d0ef1af7ff95a123be597a9db19e8a
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-24 20:25:34 -0300

    feat: boundaries

---
+++ backend/Core/Mappers/ExpenseMapper.cs
@@ -0,0 +1,31 @@
+﻿using Core.Domain;
+using Core.Dto;
+using Core.UseCase.NewExpenseUseCase.Boundaries;
+
+namespace Core.Mappers;
+
+public static class ExpenseMapper
+{
+    public static Expense MapToEntity(this NewExpenseInput input)
+    {
+        return new Expense(
+            input.Title,
+            input.Value,
+            input.Type,
+            input.Date,
+            input.UserId
+        );
+    }
+
+    public static ExpenseDto MapToDto(this Expense expense)
+    {
+        return new ExpenseDto(
+            expense.Id,
+            expense.Title,
+            expense.Value,
+            expense.Description,
+            expense.Type,
+            expense.Date
+        );
+    }
+}
---
+++ backend/Core/UseCase/NewExpenseUseCase/Boundaries/NewExpenseInput.cs
@@ -0,0 +1,14 @@
+﻿using Core.Commons;
+using Core.Domain.Enums;
+using MediatR;
+
+namespace Core.UseCase.NewExpenseUseCase.Boundaries;
+
+public sealed record NewExpenseInput(
+    string Title,
+    decimal Value,
+    string? Description,
+    ExpenseType Type,
+    DateTime Date,
+    Guid UserId
+) : IRequest<Output>;
