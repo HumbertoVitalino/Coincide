e06a201db6c5153a4836f57e170850b9853378e9
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-25 21:53:49 -0300

    feat: useCase

---
+++ backend/Core/UseCase/GetAllExpensesUseCase/GetAllExpenses.cs
@@ -0,0 +1,30 @@
+﻿using Core.Commons;
+using Core.Interfaces;
+using Core.Mappers;
+using Core.UseCase.GetAllExpensesUseCase.Boundaries;
+using MediatR;
+
+namespace Core.UseCase.GetAllExpensesUseCase;
+
+public class GetAllExpenses(
+    IExpenseRepository expenseRepository    
+) : IRequestHandler<GetAllExpensesInput, Output>
+{
+    private readonly IExpenseRepository _expenseRepository = expenseRepository;
+
+    public async Task<Output> Handle(GetAllExpensesInput input, CancellationToken cancellationToken)
+    {
+        Output output = new();
+
+        var expenses = await _expenseRepository.GetAll(input.UserId);
+
+        if (!expenses.Any() || expenses is null)
+        {
+            output.AddErrorMessage("No expenses found for the user.");
+            return output;
+        }
+
+        output.AddResult(expenses.MapToDto());
+        return output;
+    }
+}
