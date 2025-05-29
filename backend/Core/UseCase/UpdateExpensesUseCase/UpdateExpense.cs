528f99c51850049710d0902e09a36217a7d97cb0
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-05-28 22:48:54 -0300

    feat: update entity use case

--- backend/Core/UseCase/UpdateExpensesUseCase/UpdateExpense.cs
+++ backend/Core/UseCase/UpdateExpensesUseCase/UpdateExpense.cs
@@ -1,5 +1,41 @@
-﻿namespace Core.UseCase.UpdateExpensesUseCase;
+﻿using Core.Commons;
+using Core.Interfaces;
+using Core.Mappers;
+using Core.UseCase.UpdateExpensesUseCase.Boundaries;
+using MediatR;
 
-public class UpdateExpense 
+namespace Core.UseCase.UpdateExpensesUseCase;
+
+public class UpdateExpense(
+    IExpenseRepository expenseRepository,
+    IUnitOfWork unitOfWork
+) : IRequestHandler<UpdateExpenseInput, Output>
 {
+    private readonly IExpenseRepository _expenseRepository = expenseRepository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task<Output> Handle(UpdateExpenseInput request, CancellationToken cancellationToken)
+    {
+        Output output = new();
+
+        var expense = await _expenseRepository.GetAsync(request.Id);
+
+        if (expense is null || expense.UserId != request.UserId)
+        {
+            output.AddErrorMessage("Expense not found.");
+            return output;
+        }
+
+        expense.Title = request.Title;
+        expense.Value = request.Value;
+        expense.Description = request.Description;
+        expense.Type = request.Type;
+        expense.Date = request.Date;
+
+        _expenseRepository.Update(expense);
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        output.AddResult(expense.MapToDto());
+        return output;
+    }
 }
